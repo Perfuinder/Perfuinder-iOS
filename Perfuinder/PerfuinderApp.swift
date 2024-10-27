15e8dfcc72286bf14637aec8e103c6ccacab58cf
msseock <msseock@gmail.com>
2024-10-28 01:39:48 +0900

    Initial commit

---
+++ Perfuinder/PerfuinderApp.swift
@@ -0,0 +1,17 @@
+//
+//  PerfuinderApp.swift
+//  Perfuinder
+//
+//  Created by 석민솔 on 10/28/24.
+//
+
+import SwiftUI
+
+@main
+struct PerfuinderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ PerfuinderTests/PerfuinderTests.swift
@@ -0,0 +1,17 @@
+//
+//  PerfuinderTests.swift
+//  PerfuinderTests
+//
+//  Created by 석민솔 on 10/28/24.
+//
+
+import Testing
+@testable import Perfuinder
+
+struct PerfuinderTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
