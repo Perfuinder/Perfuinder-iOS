ff947b973044f6a2128c097031a724b76e8038a0
msseock <msseock@gmail.com>
2024-11-20 19:13:10 +0900

    💄 Style: 홈화면 이미지 잘 보이도록 수정
    - 포맨트 이미지: 계절랜덤에서는 캡슐 형태로 보이도록, 리스트에서는 모서리 둥글게 전체 이미지 보이도록 설정
    - 텍스트 정렬 일부 수정

--- Perfuinder/Presentation/Home/ViewModel/HomeViewModel.swift
+++ Perfuinder/Presentation/Home/ViewModel/HomeViewModel.swift
@@ -55,20 +55,21 @@ class HomeViewModel: ObservableObject {
         var randomBrandPerfumeArray: [RandomBrandPerfume] = []
         
         for i in 0...9 {
-            randomBrandPerfumeArray.append(RandomBrandPerfume(id: i, name: "향수\(i)", desc: "이제 막 수확한 월계수 잎의 신선함에 톡 쏘는 블랙베리 과즙을 가미한,  매력적이고 생기 넘치는 상쾌한 느낌의 향", imageURL: "https://www.jomalone.co.kr/media/export/cms/products/1000x1000/jo_sku_L32R01_1000x1000_0.png"))
+            randomBrandPerfumeArray.append(RandomBrandPerfume(id: i, name: "향수\(i)", desc: "이제 막 수확한 월계수 잎의 신선함에 톡 쏘는 블랙베리 과즙을 가미한,  매력적이고 생기 넘치는 상쾌한 느낌의 향", imageURL: "https://theforment.com/web/product/big/202405/2c487c7b7d6a070534b96892988cd2fe.jpg"))
         }
 
-        let seasonimageURL = "https://image.sivillage.com/upload/C00001/goods/org/548/231108006981548.jpg?RS=750&SP=1"
+        let seasonimageURL = "https://theforment.com/web/product/big/202405/2c487c7b7d6a070534b96892988cd2fe.jpg"
         
         data = HomeModel(seasonRandom:
-                                SeasonPerfume(season: SeasonCode.fall,
+                                [SeasonPerfume(season: SeasonCode.fall,
                                               perfumeId: 0,
-                                              brand: "딥디크",
+                                              brand: "FORMENT",
                                               perfumeName: "오 드 퍼퓸 오르페옹",
-                                              imageURL: seasonimageURL),
-                              randomBrandName: "조말론",
+                                              imageURL: seasonimageURL)],
+                              randomBrandName: "JOMALONE",
                               brandRandom: randomBrandPerfumeArray)
-        */
+         */
+        
     }
 }
 
