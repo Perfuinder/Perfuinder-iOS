be59c3b7c6d98eb3be6236a5031b41b6b8247f02
SSOL <msseock@gmail.com>
2024-11-21 16:03:47 +0900

    ✨ Feat: AI 탐색 키워드 선택 UI 구현 #11
    - 직접 입력 키워드: 추가 버튼 누르면 제일 앞쪽에 삭제 가능한 토큰으로 추가
    - 선택 키워드: 앱에서 기본적으로 제공되는 20개 키워드. 선택 시 토큰 색반전
    - 더보기/줄이기 버튼: 초기 UI에서는 선택 키워드는 10개만 보이도록 설정, 더보기 버튼 누르면 전체 20개 토큰 보여주고, 전체 키워드가 보일 때는 줄이는 기능하는 버튼

---
+++ Perfuinder/Presentation/Search/Model/CustomSearchModel.swift
@@ -0,0 +1,32 @@
+//
+//  CustomSearchModel.swift
+//  Perfuinder
+//
+//  Created by 석민솔 on 11/21/24.
+//
+
+import Foundation
+
+enum SelectKeyword: String, CaseIterable {
+    case sophisticated = "세련된"
+    case bold = "대담한"
+    case neutral = "중성적인"
+    case exotic = "이국적인"
+    case mature = "성숙한"
+    case pure = "순수한"
+    case adventurous = "모험적인"
+    case luxurious = "고급스러운"
+    case comfortable = "편안한"
+    case vibrant = "활기찬"
+    case mysterious = "미스터리"
+    case intense = "강렬한"
+    case romantic = "로맨틱한"
+    case urban = "도시적인"
+    case natural = "자연적인"
+    case classic = "클래식"
+    case modern = "모던"
+    case fresh = "신선한"
+    case sensual = "관능적인"
+    case elegant = "우아한"
+    case feminine = "여성적인"
+}
--- Perfuinder/Presentation/Search/View/CustomSearch.swift
+++ Perfuinder/Presentation/Search/View/CustomSearch.swift
@@ -9,15 +9,180 @@ import SwiftUI
 
 /// AI 탐색
 struct CustomSearch: View {
+    // MARK: - Properties
+    /// 직접 입력하는 키워드 String
+    @State var customKeyword: String = ""
+    
+    /// 직접 입력해서 추가한 키워드
+    @State var customKeywordList: [String] = []
+    
+    /// 기본 선택지 키워드들 중에 선택한 거
+    @State var selectedKeywordList: [SelectKeyword] = []
+    
+    /// 더보기 버튼 누르면 선택지 키워드 20종 전부 다 보여주도록 결정해주는 변수
+    @State var showAllSelectKeywords: Bool = false
+    
+    // MARK: - View
     var body: some View {
         VStack {
-            Text("AI 탐색")
+            // 키워드 입력 관련
+            selectKeywordSection
             
         }
+        .padding(.horizontal, 20)
         .toolbarVisibility(.hidden, for: .tabBar)   // 탭바 안보이도록
     }
 }
 
 #Preview {
     CustomSearch()
+}
+
+// MARK: - View Parts
+extension CustomSearch {
+    // MARK: 키워드
+    /// 키워드 입력하는 부분 UI 전체
+    private var selectKeywordSection: some View {
+        VStack(spacing: 0) {
+            // 섹션 제목
+            Text("키워드")
+                .font(.title3)
+                .fontWeight(.semibold)
+                .frame(maxWidth: .infinity, alignment: .leading)
+                .padding(.bottom, 20)
+            
+            // 키워드 직접 입력하는 부분
+            customKeywordInput
+                .padding(.bottom, 10)
+            
+            WrapLayout {
+                // 직접 입력한 키워드 제일 먼저 보여주기
+                ForEach(customKeywordList, id: \.self) { text in
+                    customKeywordToken(text)
+                }
+                
+                // 화면에서 보여줄 키워드 선택: 기본 키워드 10개, 모두 보기하면 20개
+                let selectableKeywords = showAllSelectKeywords ? SelectKeyword.allCases : Array(SelectKeyword.allCases.prefix(10))
+                
+                // 선택지 키워드 토큰들
+                ForEach(selectableKeywords, id: \.self) { keyword in
+                    selectKeywordToken(keyword)
+                }
+                
+                // 더보기/줄이기 버튼
+                wrapButton
+            }
+        }
+    }
+    
+    /// 키워드 집접 입력하기 부분
+    private var customKeywordInput: some View {
+        HStack {
+            TextField("키워드 직접 입력하기", text: $customKeyword)
+                .multilineTextAlignment(.center)
+                .padding(.horizontal, 15)
+                .padding(.vertical, 10)
+                .background(
+                    Color.unselected
+                )
+                .clipShape(Capsule())
+            
+            Button {
+                withAnimation (.easeInOut) {
+                    // 텍스트필드에 있는 거 label로 내리기
+                    customKeywordList.insert(customKeyword, at: 0)
+                    
+                    // 텍스트필드 비우기
+                    customKeyword.removeAll()
+                }
+                
+            } label: {
+                HStack(spacing: 2) {
+                    Image(systemName: "plus")
+                    Text("추가")
+                }
+                .font(.callout)
+                .foregroundStyle(Color.secondary)
+                .padding(.horizontal, 15)
+                .padding(.vertical, 10)
+                .background(Color.unselected)
+                .clipShape(Capsule())
+            }
+
+        }
+    }
+    
+    /// 직접 입력한 키워드 토큰
+    @ViewBuilder
+    private func customKeywordToken(_ text: String) -> some View {
+        HStack {
+            // 텍스트
+            Text(text)
+                .foregroundStyle(Color.black)
+            
+            // 지우기 버튼
+            Button {
+                withAnimation(.easeInOut) {
+                    // 직접 입력 키워드에서 삭제
+                    customKeywordList.removeAll { $0 == text }
+                }
+            } label: {
+                Image(systemName: "multiply")
+                    .foregroundStyle(Color.secondary)
+            }
+        }
+        .font(.callout)
+        .padding(.horizontal, 15)
+        .padding(.vertical, 10)
+        .background(
+            RoundedRectangle(cornerRadius: 40)
+                .fill(Color.unselected)
+                .stroke(Color.customBlack, lineWidth: 1)
+        )
+    }
+    
+    /// 선택지 키워드 토큰
+    @ViewBuilder
+    private func selectKeywordToken(_ keyword: SelectKeyword) -> some View {
+        Text(keyword.rawValue)
+            .font(.callout)
+            .padding(.horizontal, 15)
+            .padding(.vertical, 10)
+            // 글씨) 선택 ? 흰색 : 검은색
+            .foregroundStyle(selectedKeywordList.contains(keyword) ? Color.white : Color.black)
+            // 배경) 선택 ? 검은색 : 흰색
+            .background(
+                RoundedRectangle(cornerRadius: 40)
+                    .fill(selectedKeywordList.contains(keyword) ? Color.customBlack : Color.unselected)
+            )
+            .onTapGesture {
+                withAnimation (.easeInOut) {
+                    // 이미 선택한 키워드면 없애주기
+                    if selectedKeywordList.contains(keyword) {
+                        selectedKeywordList.removeAll(where: {$0.rawValue == keyword.rawValue})
+                    }
+                    // 기존 선택 키워드들 중에 없었으면 추가하기
+                    else {
+                        selectedKeywordList.append(keyword)
+                    }
+                }
+            }
+    }
+    
+    /// 더보기 버튼 토큰
+    private var wrapButton: some View {
+        Button {
+            withAnimation(.easeInOut) {
+                showAllSelectKeywords.toggle()
+            }
+        } label: {
+            Image(systemName: showAllSelectKeywords ? "chevron.up" : "chevron.down")
+                .font(.callout)
+                .foregroundStyle(Color.black)
+                .padding(.horizontal, 15)
+                .padding(.vertical, 15)
+                .background(Color.main10)
+                .clipShape(Capsule())
+        }
+    }
 }
